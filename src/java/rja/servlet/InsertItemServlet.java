0e6dab806b75bf325c683c4339e53c7396898025
Rajesh J Advani <rajeshjadvani@yahoo.com>
2009-06-15 00:54:34 +0530

    Working build. Servlet code incomplete.

---
+++ src/java/rja/servlet/InsertItemServlet.java
@@ -0,0 +1,22 @@
+package rja.servlet;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Describe class InsertItemServlet here.
+ *
+ *
+ * Created: Mon Jun 15 00:46:17 2009
+ *
+ * @author <a href="mailto:rajeshja@rja-desktop-lnx">Rajesh</a>
+ * @version 1.0
+ */
+public class InsertItemServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+		
+	}
+
+}
