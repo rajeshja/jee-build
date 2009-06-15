80b2bfa26e7db2eb75041de172f3627e5473378b
RAJESH J ADVANI(WT01 - Retail <rajeshja@GDYLPT13432.wipro.com>
2009-06-15 20:43:38 +0530

    Code changes and ds file to get EAR working and deployed. Item entity is not working though.

--- src/java/rja/servlet/InsertItemServlet.java
+++ src/java/rja/servlet/InsertItemServlet.java
@@ -1,8 +1,20 @@
 package rja.servlet;
 
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.persistence.Query;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import rja.entities.Item;
+
 
 /**
  * Describe class InsertItemServlet here.
@@ -15,8 +27,39 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class InsertItemServlet extends HttpServlet {
 
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
+	@PersistenceUnit(unitName="j2eeDB")
+	EntityManagerFactory emf;
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+		
+		PrintWriter out = response.getWriter();
+		out.print("<html><head><title>InsertItem</title></head><body>");
+
+		EntityManager em = emf.createEntityManager();
+
+		Item item = new Item();
+		item.setName("Item " + (int)(Math.random()*100));
+		item.setDescription("Random description");
+		item.setCreatedDate(new Date());
+		
+		em.persist(item);
+
+		Query query = em.createQuery("select from Item");
+		List<Item> items = query.getResultList();
+
+		for (Item i: items) {
+			out.print(i.getName() + "<br/>");
+		}
+		
+		out.print("</body></html>");
 		
 	}
+
+	public void doPost(HttpServletRequest request, HttpServletResponse response) 
+		throws ServletException, IOException {
+
+		doGet(request, response);
+	}	
 
 }
