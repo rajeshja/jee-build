d6399d5bdb238aaf8e2aa5a180d09aaeaec0732b
Rajesh J Advani <rajeshjadvani@yahoo.com>
2009-06-18 01:27:57 +0530

    Moved html to a JSP which uses JSTL to display all the data.

--- src/java/rja/servlet/InsertItemServlet.java
+++ src/java/rja/servlet/InsertItemServlet.java
@@ -1,15 +1,10 @@
 package rja.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.math.BigDecimal;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.PersistenceUnit;
-import javax.persistence.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -40,9 +35,6 @@ public class InsertItemServlet extends HttpServlet {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) 
 		throws ServletException, IOException {
 		
-		PrintWriter out = response.getWriter();
-		out.print("<html><head><title>InsertItem</title></head><body>");
-
 		Item item = new Item();
 		item.setName("Item " + (int)(Math.random()*100));
 		item.setDescription("Random description");
@@ -50,30 +42,19 @@ public class InsertItemServlet extends HttpServlet {
 		item.setPrice(new BigDecimal(100.0));
 		
 		itemManager.insert(item);
-		
-		out.print("Just attempted to create an item - " 
-				  + item.getName() + ", "
-				  + item.getDescription() + ", "
-				  + item.getCreatedDate() + ", "
-				  + item.getPrice() + "<br/>");
-		out.println();
 
+		request.setAttribute("itemInserted", item);
+		
 		List<Item> items = itemManager.findAllItems();
-		
-		if (items!=null) {
-			out.print("The query returned " + items.size() + " rows.<br/>");
-		} else {
-			out.print("The query returned null.<br/>");
-		}
-		
-		for (Item i: items) {
-			out.print(i.getName() + "<br/>");
-		}
 
-		out.print("And the sum of 2 and 3 is " + calculator.add(2,3) + "<br/>");
-		out.print("And the different of 5 and 3 is " + calculator.subtract(5,3) + "<br/>");
+		request.setAttribute("items", items);
+
+		request.setAttribute("sum", calculator.add(2,3));
+		request.setAttribute("difference", calculator.subtract(5,2));
+
+		request.getRequestDispatcher("item-list.jsp").forward(request, response);
+
 		
-		out.print("</body></html>");
 		
 	}
 
---
+++ src/web/item-list.jsp
@@ -0,0 +1,27 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Items Page</title>
+</head>
+<body>
+	<h1>Items Page</h1>
+	<c:if test="${empty requestScope.itemInserted}" >
+		Just attempted to create an item - 
+		${requestScope.itemInserted.name}, 
+		${requestScope.itemInserted.description}, 
+		${requestScope.itemInserted.createdDate}, 
+		${requestScope.itemInserted.price} 
+	</c:if>
+	<br/>
+	<c:if test="${empty requestScope.items}" >
+		The query returned ${requestScope.items.size} items.<br/>
+	</c:if>
+	<c:forEach var="item" items="${requestScope.items}">
+		Item: ${item.name} <br/>
+	</c:forEach>
+	<br/>
+	Sum of 2 and 3 is ${requestScope.sum}. <br/>
+	Difference of 5 and 2 is ${requestScope.difference}. <br/>
+
+</body>
+</html>
